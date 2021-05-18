c54d1fa9b3e78fb01a3be38e8ae6b8d9a8132555
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-17 17:27:45 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by user on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
