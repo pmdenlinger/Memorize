e091721a44c0700b09c2019d7a9d4d3de5a9e58d
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-20 10:28:24 -0700

    Create four ZStacks to embed in HStack.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World")
+                    .foregroundColor(.orange)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World")
+                    .foregroundColor(.orange)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World")
+                    .foregroundColor(.orange)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World")
+                    .foregroundColor(.orange)
+            }
+           
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
