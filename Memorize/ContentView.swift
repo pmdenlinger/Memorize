e54671b4daa6f903b368b6f88dedfdd76700bfc2
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-21 08:25:08 -0700

    Add content to CardView() struct, create emoji array to hold emojis

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚂", "🚀", "🚁", "🛻"]
     var body: some View {
         HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+            CardView(content:"🚂")
+            CardView(content:"🚀")
+            CardView(content:"🚁")
+            CardView(content: "🛻")
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
@@ -21,6 +22,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = true
     
     var body: some View {
@@ -29,7 +31,7 @@ struct CardView: View {
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
             shape.stroke(lineWidth: 3.0)
-            Text("🚂")
+            Text(content)
                 .font(.largeTitle)
             } else {
                 shape.fill()
